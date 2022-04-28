6e3dca4b2bfb2bf871aab886f7b220bbe332a3bf
FranRSantander <104594052+FranRSantander@users.noreply.github.com>
2022-04-28 17:22:00 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 8080
+///////////////////////////////////////
+app.set('view engine', 'ejs');
+
+app.set('views','./views');
+
+app.use(express.static('public'))
+
+
+app.listen(PORT,()=>{
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+})
