ead6784d5cb7701dc89982f0937d681be9381add
Rechou <A446464@santandertecnologia.com.ar>
2022-04-29 10:18:30 -0300

    Ruteando app

--- app.js
+++ app.js
@@ -9,7 +9,28 @@ app.set('view engine', 'ejs');
 app.set('views','./views');
 
 app.use(express.static('public'))
+/////////////////////////////////////
+app.get('/',(req, res)=>{
+    res.render('pages/index')
+})
 
+app.get('/products',(req, res)=>{
+    res.render('pages/product')
+})
+
+app.get('/cart',(req, res)=>{
+    res.render('pages/cart')
+})
+
+app.get('/checkout',(req, res)=>{
+    res.render('pages/checkout')
+})
+app.get('/register',(req, res)=>{
+    res.render('pages/register')
+})
+app.get('/login',(req, res)=>{
+    res.render('pages/login')
+})
 
 app.listen(PORT,()=>{
     console.log(`Servidor corriendo en el puerto ${PORT}`);
