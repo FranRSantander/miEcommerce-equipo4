d5030a2d159b94efb91d2047f8de819c674aaa10
Rechou <A446464@santandertecnologia.com.ar>
2022-04-29 10:33:02 -0300

    Ruteando app ahora con cierres

--- app.js
+++ app.js
@@ -2,36 +2,38 @@ const express = require('express');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 ///////////////////////////////////////
 app.set('view engine', 'ejs');
 
 app.set('views','./views');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 /////////////////////////////////////
 app.get('/',(req, res)=>{
-    res.render('pages/index')
-})
+    res.render('pages/index');
+});
 
 app.get('/products',(req, res)=>{
-    res.render('pages/product')
-})
+    res.render('pages/product');
+});
 
 app.get('/cart',(req, res)=>{
-    res.render('pages/cart')
-})
+    res.render('pages/cart');
+});
 
 app.get('/checkout',(req, res)=>{
-    res.render('pages/checkout')
-})
-app.get('/register',(req, res)=>{
-    res.render('pages/register')
-})
-app.get('/login',(req, res)=>{
-    res.render('pages/login')
-})
+    res.render('pages/checkout');
+});
 
+app.get('/register',(req, res)=>{
+    res.render('pages/register');
+});
+
+app.get('/login',(req, res)=>{
+    res.render('pages/login');
+});
+///////////////////////////////////////
 app.listen(PORT,()=>{
     console.log(`Servidor corriendo en el puerto ${PORT}`);
-})
+});
