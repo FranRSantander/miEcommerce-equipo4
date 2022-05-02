3193fb7ef74b44a77850ac97c585f0a338523eba
PabloSerradellSantander <pserradell@santandertecnologia.com.ar>
2022-05-02 11:40:21 -0300

    subiendo carrito en mi rama

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 app.set('views','./views');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 /////////////////////////////////////
 app.get('/',(req, res)=>{
     res.render('pages/index')
