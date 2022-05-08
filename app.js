6d9ec6930e3c45e284a593d4a2b7d71e0c1364e7
Rechou <A446464@santandertecnologia.com.ar>
2022-05-08 15:28:36 -0300

    Arreglando css

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+var createError = require('http-errors');
+
 const app = express();
 
 const PORT = process.env.PORT || 8080;
@@ -15,15 +17,20 @@ app.set('view engine', 'ejs');
 app.set('views','./views/pages');
 
 app.use(express.static('public'));
-
 /////////////////////////////////////
+
 app.use('/', indexRoutes);
 
 app.use('/users', usersRoutes);
 
 app.use('/', productsRoutes);
-
 ///////////////////////////////////////
 app.listen(PORT,()=>{
     console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+// catch 404 and forward to error handler
+
+app.use(function(req, res, next) {
+    next(createError(404));
 });
