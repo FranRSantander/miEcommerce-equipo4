5f88ef720f9ae9d282772fbe1611f025e4888498
PabloSerradellSantander <pserradell@santandertecnologia.com.ar>
2022-05-09 15:57:34 -0300

    pequeños cambios que en rama de franco estan mejor hechos

--- controllers/products.js
+++ controllers/products.js
@@ -1,41 +1,66 @@
 const fetch = require('node-fetch');
 const urlBase = 'https://fakestoreapi.com';
 
+const orderByRate = (products) => {
+    products.sort((obj1,obj2)=>{
+        if(obj1.rating.rate < obj2.rating.rate){
+            return 1;
+        }else if(obj1.rating.rate > obj2.rating.rate){
+            return -1;
+        }else{
+            return 0;
+        }
+    });
+}
 
-const getAllProducts = (req, res)=>{
+const getAllProducts = async (req, res)=>{
 
-    fetch(`${urlBase}/products`)
-    .then(response => response.json())
-    .then(products => {
+    try {
+        const response = await fetch(`${urlBase}/products`);
+        const products = await response.json();
 
-        products.sort((obj1,obj2)=>{
-            if(obj1.rating.rate < obj2.rating.rate){
-                return 1;
-            }else if(obj1.rating.rate > obj2.rating.rate){
-                return -1;
-            }else{
-                return 0;
-            }
-        });
-        
+        orderByRate(products);
+
         res.render('index', {products});
-    })
-    .catch(err => console.log(err));
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    // fetch(`${urlBase}/products`)
+    // .then(response => response.json())
+    // .then(products => {
+
+    //     orderByRate(products);
+        
+    //     res.render('index', {products});
+    // })
+    // .catch(err => console.log(err));
 };
 
 const getProduct = async (req, res)=>{
 
     const {id} = req.params;
     try {
-        const response = await fetch(`${urlBase}/products/${id}`);
-        const product = await response.json();
-        res.render('product', {product});
+        const response = await fetch(`${urlBase}/products`);
+        const products = await response.json();
+
+        orderByRate(products);
+
+        const product = products.find(product => product.id == id);
+        
+        if(product === undefined){
+            res.send('Ese producto no esta en la base de datos');
+        }else{
+            res.render('product', {products: products, product});
+        }
 
     } catch (error) {
         console.log(error);
     }
 
 };
+
 
 module.exports = {
     getAllProducts,
