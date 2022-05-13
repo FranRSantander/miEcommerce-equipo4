fea2df90892abe9139856b2db1beff854c4c08ba
PabloSerradellSantander <pserradell@santandertecnologia.com.ar>
2022-05-12 23:13:03 -0300

    cambie un Promise.all en controlador de productos, agregué comentarios y una verificación en línea 59

--- controllers/products.js
+++ controllers/products.js
@@ -27,6 +27,7 @@ const getSuggested = async (req, res) => {
     }
 };
 
+// Funcion asincrónica para renderizar en el index los sugeridos y los más pedidos:
 const getProducts = (req, res) => {
 
     Promise.all([getMostWanted(), getSuggested()])
@@ -45,22 +46,24 @@ const getProduct = async (req, res) => {
     const {id} = req.params;
     try {
         const response = await fetch(`${urlBase}/products/`);
-        const products = await response.json();
+        const AllProducts = await response.json();
 
-        const product = products.find(product => product._id == id);
+        // Verifico que el id que llega por params sea un id válido en la lista de productos que tiene la api:
+        const product = AllProducts.find(product => product._id == id);
         if(product === undefined){
             res.status(404).render('404product');
         }
         else{
-            Promise.all([getMostWanted(), getSuggested()])
-            .then(
-                values => {
-                    let mostwanted = values[0];
-                    let suggested = values[1];
-                    
-                    res.render('product', {suggested, product});
+            getSuggested()
+            .then(suggested => {
+                // Si el producto del que se quiere ver su detalle es uno de los productos sugeridos, lo saco de este arreglo, así no se lo vuelve a sugerir debajo:
+                let productIndex = suggested.findIndex(oneProduct => oneProduct._id == product._id);
+                if(productIndex > -1 && productIndex < 4){
+                    suggested.splice(productIndex, 1);
                 }
-            );
+                
+                res.render('product', {suggested, product});
+            });
         }
     } catch (error) {
         console.log(error);
