e14d22f968f8dc7f0b7276a78b51075d744889f0
cfkuba <ckuba@santandertecnologia.com.ar>
2022-05-16 10:19:26 -0300

    limpiar validaciones

--- middlewares/validations.js
+++ middlewares/validations.js
@@ -3,39 +3,26 @@ const fs = require("fs");
 
 const validationsRegister = [
     check("email")
-      .notEmpty()
-      .withMessage("Debe ingresar un email")
+      .notEmpty().withMessage("Debe ingresar un email")
       .bail()
-      .isEmail()
-      .withMessage("Debe ingresar un email válido"),
+      .isEmail().withMessage("Debe ingresar un email válido"),
     check("password1")
-      .notEmpty()
-      .withMessage("Debe ingresar una contraseña")
+      .notEmpty().withMessage("Debe ingresar una contraseña")
       .bail()
-      .isLength({ min: 8 })
-      .withMessage("La contraseña debe tener 8 caracteres como mínimo").bail()
-      .trim()
+      .isLength({ min: 8 }).withMessage("La contraseña debe tener 8 caracteres como mínimo")
       .bail()
       .isNumeric().withMessage("La contraseña debe ser numérica"),
-    check("password2")
-      .notEmpty()
-      .withMessage("Debe ingresar una contraseña")
+     check("password2")
+      .notEmpty().withMessage("Debe ingresar una contraseña")
       .bail()
-      .isLength({ min: 8 })
-      .withMessage("La contraseña debe tener 8 caracteres como mínimo")
-      .custom((value,{req}) => {                                             
-          const password1 = req.body.password1;
-          if (value !== password1) {
-            throw new Error("La confirmación de la contraseña debe ser igual a la contraseña");
-          }
-        })
-      .bail()
-      .trim()  
+      .isLength({ min: 8 }).withMessage("La contraseña debe tener 8 caracteres como mínimo")
       .bail()
       .isNumeric().withMessage("La contraseña debe ser numérica")
+        
+       
   ];
 
-  let validationsLogin = [
+   let validationsLogin = [
     check("email")
     .notEmpty()
     .withMessage("Debe ingresar un email")
@@ -44,6 +31,6 @@ const validationsRegister = [
     .withMessage("Debe ingresar un email válido")
     
   ]
-  
+   
 
   module.exports = {validationsRegister, validationsLogin}
