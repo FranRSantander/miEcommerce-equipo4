fa0ea0e8bc6d8f2617b6e47d6d3481d432974d95
Rechou <A446464@santandertecnologia.com.ar>
2022-05-16 14:30:03 -0300

    Remove ProductJs

--- public/js/products.js
+++
@@ -1,6 +0,0 @@
-//Funcion que permite cambiar las imagenes de la vista producto
-
-function cambiarImgProducto(selectImg){
-    let bigImg = document.getElementById("mainImg");
-    bigImg.src = selectImg.src;
-}
