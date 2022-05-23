49b3bafb2bbf54be24a571178eae049629a6648e
cfkuba <ckuba@santandertecnologia.com.ar>
2022-05-23 12:03:23 -0300

    hide show del menu ppal

--- public/js/menuPrincipal.js
+++ public/js/menuPrincipal.js
@@ -5,29 +5,7 @@ let menuElement = document.querySelector("#menu-perfil");
 //click en boton de Perfil, aparece menú
 
 iniciarSesionBoton.addEventListener("click", (e)=>{
-    e.preventDefault();
-    if(menuElement.classList.contains("hide")){
-        menuElement.classList.add("show"); 
-        menuElement.classList.remove("hide"); 
-    } /* else{
-        menuElement.classList.add("hide"); 
-        menuElement.classList.remove("show");
-    }   */
-    
+   e.preventDefault();
+    menuElement.classList.toggle("hide");
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 })
